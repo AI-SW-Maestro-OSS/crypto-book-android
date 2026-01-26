5ced09af04ca49c049da08fe817d3e71d436ecf9
mingkang <mingkang@student.42seoul.kr>
2026-01-26 11:33:14 -0800

    refactor(coin-img): Move CoinImageResolverImpl to core data module
    Moves the `CoinImageResolverImpl` from the `home:data` module to the `core:data` module. This centralizes the coin image resolving logic, making it accessible to other features in the future.
    
    The dependencies in the Dagger module have been updated to reflect this change. Additionally, an unused `LocalContext` import was removed from `CoinListItem`.

--- home/presentation/src/main/java/io/soma/cryptobook/home/presentation/component/coinlist/CoinListItem.kt
+++ home/presentation/src/main/java/io/soma/cryptobook/home/presentation/component/coinlist/CoinListItem.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -25,8 +24,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 import io.soma.cryptobook.core.designsystem.R
 import io.soma.cryptobook.core.designsystem.theme.PriceDown
 import io.soma.cryptobook.core.designsystem.theme.PriceFlat
