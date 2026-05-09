11dc3363dff43833364a969df781b6bdc3f338de
helpingstar <iamhelpingstar@gmail.com>
2026-05-10 03:44:28 +0900

    feat: scaffold search feature modules

---
+++ search/presentation/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.cryptobook.android.presentation)
+    alias(libs.plugins.cryptobook.android.library.compose)
+    alias(libs.plugins.cryptobook.spotless)
+}
+
+android {
+    namespace = "io.soma.cryptobook.search.presentation"
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
