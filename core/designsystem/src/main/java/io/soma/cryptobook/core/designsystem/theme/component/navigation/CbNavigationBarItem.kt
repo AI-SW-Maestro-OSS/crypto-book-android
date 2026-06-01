789ea29e42b37335f5dc7de830157e0a67b72d22
helpingstar <iamhelpingstar@gmail.com>
2026-06-01 12:21:52 +0900

    fix: apply secondary icon tint to selected navigation bar item

--- core/designsystem/src/main/java/io/soma/cryptobook/core/designsystem/theme/component/navigation/CbNavigationBarItem.kt
+++ core/designsystem/src/main/java/io/soma/cryptobook/core/designsystem/theme/component/navigation/CbNavigationBarItem.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -34,7 +33,7 @@ fun RowScope.CbNavigationBarItem(
                     ),
                     contentDescription = null,
                     tint = if (isSelected) {
-                        Color.Unspecified
+                        CbTheme.colorScheme.icon.secondary
                     } else {
                         CbTheme.colorScheme.icon.primary
                     }
