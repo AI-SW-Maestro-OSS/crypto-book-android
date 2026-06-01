bf56e3a72b2a34016ce6053d24815f522d52b895
helpingstar <iamhelpingstar@gmail.com>
2026-06-01 12:33:41 +0900

    fix: add bottom spacing and navigation bar padding to coin detail screen

--- coin-detail/presentation/src/main/java/io/soma/cryptobook/coindetail/presentation/CoinDetailScreen.kt
+++ coin-detail/presentation/src/main/java/io/soma/cryptobook/coindetail/presentation/CoinDetailScreen.kt
@@ -3,9 +3,11 @@ package io.soma.cryptobook.coindetail.presentation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -188,6 +190,9 @@ private fun CoinDetailContent(state: State, modifier: Modifier = Modifier) {
             rows = state.orderBook.rows,
             onTickSizeClick = {},
         )
+
+        Spacer(modifier = Modifier.height(height = 16.dp))
+        Spacer(modifier = Modifier.navigationBarsPadding())
     }
 }
 
