861e89a95088094af181bd7ce7791048e2f44ca1
helpingstar <iamhelpingstar@gmail.com>
2026-07-12 00:37:59 +0900

    fix: request only TRADING symbols from Binance 24hr ticker

--- home/data/src/main/java/io/soma/cryptobook/home/data/network/BinanceApiService.kt
+++ home/data/src/main/java/io/soma/cryptobook/home/data/network/BinanceApiService.kt
@@ -3,8 +3,11 @@ package io.soma.cryptobook.home.data.network
 import io.soma.cryptobook.home.data.model.BinanceTickerDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BinanceApiService {
     @GET("api/v3/ticker/24hr")
-    suspend fun getAllTickerPrices(): Response<List<BinanceTickerDto>>
+    suspend fun getAllTickerPrices(
+        @Query("symbolStatus") symbolStatus: String = "TRADING",
+    ): Response<List<BinanceTickerDto>>
 }
