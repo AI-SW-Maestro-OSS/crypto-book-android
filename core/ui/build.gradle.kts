bb023ed7841e14fae2105a26aac9425960ca563d
helpingstar <iamhelpingstar@gmail.com>
2026-07-14 20:33:32 +0900

    feat(core-ui): add State-Action-Event BaseViewModel and EventsEffect

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.cryptobook.android.library.compose)
+    alias(libs.plugins.cryptobook.android.presentation)
+    alias(libs.plugins.cryptobook.spotless)
+}
+
+android {
+    namespace = "io.soma.cryptobook.core.ui"
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
